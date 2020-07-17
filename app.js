67d397ed37860d146c191c04d224ca5b243fe7c4
margaret-jihua <margaret.jihua@gmail.com>
2020-07-17 14:08:41 -0700

    [variables] Add five variables

---
+++ app.js
@@ -0,0 +1,10 @@
+let str = 'apple';
+let num = 5;
+let arr = [3,4,5,6];
+let isCool = true;
+let fruits = {
+    item: 'peach',
+    count: 7,
+    color: ['white', 'yellow'],
+    isSweeet: true
+}
